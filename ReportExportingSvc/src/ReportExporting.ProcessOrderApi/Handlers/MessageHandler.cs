900f180f0a86bcde779dcfaa0ba01fa4a53ea7b0
DESKTOP-H29UOQ1\bobpham <bobpham.tdp@gmail.com>
2023-07-26 21:53:58 +1000

    work in progress

---
+++ ReportExportingSvc/src/ReportExporting.ProcessOrderApi/Handlers/MessageHandler.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace ReportExporting.ProcessOrderApi.Handlers;
+
+public class MessageHandler
+{
+    private readonly ServiceBusProcessor _processor;
+
+    public MessageHandler(ServiceBusClient serviceBusClient, IConfiguration configuration)
+    {
+        var options = new ServiceBusProcessorOptions
+        {
+            // By default after the message handler returns, the processor will complete the message
+            // If I want more fine-grained control over settlement, I can set this to false.
+            AutoCompleteMessages = false,
+
+            // I can also allow for multi-threading
+            MaxConcurrentCalls = 2
+        };
+        _processor = serviceBusClient.CreateProcessor(configuration["QueueName"], options);
+    }
+
+    public async Task Register()
+    {
+
+        _processor.ProcessMessageAsync += ReceiveMessageHandler;
+        _processor.ProcessErrorAsync += ErrorHandler;
+
+        await _processor.StartProcessingAsync();
+    }
+
+    private static async Task ReceiveMessageHandler(ProcessMessageEventArgs args)
+    {
+        var body = args.Message.Body.ToString();
+        Console.WriteLine(body);
+
+        // we can evaluate application logic and use that to determine how to settle the message.
+        await args.CompleteMessageAsync(args.Message);
+    }
+
+    private static Task ErrorHandler(ProcessErrorEventArgs args)
+    {
+        // the error source tells me at what point in the processing an error occurred
+        Console.WriteLine(args.ErrorSource);
+
+        return Task.CompletedTask;
+    }
+}
--- ReportExportingSvc/src/ReportExporting.ProcessOrderApi/Program.cs
+++ ReportExportingSvc/src/ReportExporting.ProcessOrderApi/Program.cs
@@ -1,3 +1,7 @@
+using Azure.Identity;
+using Microsoft.Extensions.Azure;
+using ReportExporting.ProcessOrderApi.Handlers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +11,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAzureClients(cfg =>
+{
+    cfg.AddServiceBusClient(builder.Configuration.GetSection("ServiceBus"))
+        .WithCredential(new DefaultAzureCredential());
+
+    cfg.AddTableServiceClient(new Uri(builder.Configuration.GetValue<string>("TableStorageServiceUrl")!))
+        .WithCredential(new DefaultAzureCredential());
+});
+
+builder.Services.AddSingleton<MessageHandler>();
+
 var app = builder.Build();
+
+
+app.Services.GetService<MessageHandler>()?.Register();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())

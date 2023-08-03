ee890c97e1a8cadd44d7d26a9cc7f7cb5c791240
DESKTOP-H29UOQ1\bobpham <bobpham.tdp@gmail.com>
2023-08-04 00:08:05 +1000

    work in progress

--- ReportExportingSvc/tests/ReportExporting.ApplicationLibTests/BlobStorageServiceTests.cs
+++ ReportExportingSvc/tests/ReportExporting.ApplicationLibTests/BlobStorageServiceTests.cs
@@ -39,4 +39,34 @@ public class BlobStorageServiceTests
         // Assert
         expectedResponse.HasValue.Should().BeTrue();
     }
+
+    [Fact]
+    public async Task DownloadExportFileAyncTest()
+    {
+        // Arrange
+        var responseMock = new Mock<Response>();
+        responseMock.SetupGet(r => r.Status).Returns(206);
+
+        var blobServiceClient = new Mock<BlobServiceClient>();
+        var blobContainerClient = new Mock<BlobContainerClient>();
+        var blobClient = new Mock<BlobClient>();
+
+        blobServiceClient.Setup(x => x.GetBlobContainerClient(It.IsAny<string>())).Returns(blobContainerClient.Object);
+
+        blobContainerClient.Setup(x => x.GetBlobClient(It.IsAny<string>()))
+            .Returns(blobClient.Object);
+
+        blobClient.Setup(x => x.DownloadToAsync(It.IsAny<MemoryStream>())).ReturnsAsync(responseMock.Object);
+
+        Mock<IConfiguration> configuration = new();
+
+        var blobStorageService = new BlobStorageService(blobServiceClient.Object, configuration.Object);
+
+        // Act
+        var expectedResponse = await blobStorageService.DownloadExportFileAync("123", new MemoryStream());
+
+
+        // Assert
+        expectedResponse.Status.Should().Be(206);
+    }
 }

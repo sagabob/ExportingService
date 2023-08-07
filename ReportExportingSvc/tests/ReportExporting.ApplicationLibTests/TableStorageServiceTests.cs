c1385703a4b62ef93bff017eaa3d5095b4313d40
DESKTOP-H29UOQ1\bobpham <bobpham.tdp@gmail.com>
2023-08-07 13:35:41 +1000

    add more tests

--- ReportExportingSvc/tests/ReportExporting.ApplicationLibTests/TableStorageServiceTests.cs
+++ ReportExportingSvc/tests/ReportExporting.ApplicationLibTests/TableStorageServiceTests.cs
@@ -1,15 +1,21 @@
 ﻿using Azure;
 using Azure.Data.Tables;
+using FluentAssertions;
 using Microsoft.Extensions.Configuration;
 using Moq;
 using ReportExporting.ApplicationLib.Entities;
+using ReportExporting.ApplicationLib.Helpers;
+using ReportExporting.ApplicationLib.Helpers.Core;
 using ReportExporting.ApplicationLib.Services.Core;
+using ReportExporting.TestHelpers;
+using Xunit;
 
 namespace ReportExporting.ApplicationLibTests;
 
 public class TableStorageServiceTests
 {
-    public void UpsertEntityAsyncTest()
+    [Fact]
+    public async Task UpsertEntityAsyncTest()
     {
         // Arrange
 
@@ -24,14 +30,29 @@ public class TableStorageServiceTests
 
         tableServiceClientMock.Setup(x => x.GetTableClient(It.IsAny<string>())).Returns(tableClientMock.Object);
 
-        tableClientMock.Setup(x => x.UpsertEntityAsync(It.IsAny<ReportRequestTableEntity>(), TableUpdateMode.Merge, default))
+        tableClientMock.Setup(x =>
+                x.UpsertEntityAsync(It.IsAny<ReportRequestTableEntity>(), TableUpdateMode.Merge, default))
             .ReturnsAsync(responseMock.Object);
 
 
         var tableStorageService =
             new TableStorageService(tableServiceClientMock.Object, configuration.Object);
 
-      
+        var request = TestDataFactory.GetFakeReportRequest();
 
+        IReportRequestTableEntityFactory tableEntityFactory = new ReportRequestTableEntityFactory();
+        IReportRequestObjectFactory reportRequestObjectFactory = new ReportRequestObjectFactory();
+
+        var reportRequestObject = reportRequestObjectFactory.CreateFromReportRequest(request);
+
+        var tableEntityObject = tableEntityFactory.CreateTableEntity(reportRequestObject);
+
+
+        // Act
+        var expectedResponse = await tableStorageService.UpsertEntityAsync(tableEntityObject);
+
+
+        // Assertion
+        expectedResponse.Status.Should().Be(200);
     }
 }
